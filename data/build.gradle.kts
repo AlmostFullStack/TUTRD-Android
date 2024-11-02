ce231bcf15630957ca68397e2a611ecd118df8da
easyhz <jiton3@naver.com>
2024-11-02 15:14:19 +0900

    chore: data 모듈 추가 (#2)

---
+++ data/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.tutrd.android.library)
+    alias(libs.plugins.tutrd.android.hilt)
+    alias(libs.plugins.tutrd.android.application.test)
+    alias(libs.plugins.tutrd.android.application.firebase)
+}
+
+android {
+    namespace = "com.easyhz.tutrd.data"
+}
+
+dependencies {
+
+}
